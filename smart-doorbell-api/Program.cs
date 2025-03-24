98cfa18d9354fea5cc9acdee503a3e8c24a9bf68
mathias <mathiasbeckj@gmail.com>
2025-03-24 09:58:55 +0100

    Added CORS and allowed all

--- smart-doorbell-api/Program.cs
+++ smart-doorbell-api/Program.cs
@@ -40,6 +40,17 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+
 builder.Services.AddAuthorization();
 builder.Services.AddAuthentication();
 
@@ -70,6 +81,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
