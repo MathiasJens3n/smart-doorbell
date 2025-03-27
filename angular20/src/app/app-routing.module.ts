ca690188dacbaa58a1e14f4ad004ae61660565bc
lahi1998 <95356006+lahi1998@users.noreply.github.com>
2025-03-27 10:57:58 +0100

    Update app-routing.module.ts

--- angular20/src/app/app-routing.module.ts
+++ angular20/src/app/app-routing.module.ts
@@ -5,25 +5,22 @@ import { CreateAccountComponent } from './Components/create-account/create-accou
 import { ImageGalleryComponent } from './Components/image-gallery/image-gallery.component';
 import { DeviceComponent } from './Components/device/device.component';
 
-// Function to check for 'Token' cookie
-const checkAuth: CanActivateFn = () => {
-  const token = document.cookie.split('; ').find(row => row.startsWith('Token='));
-  return !!token;  // Returns true if 'Token' cookie is present, otherwise false
+export const checkAuth: CanActivateFn = () => {
+  return !!sessionStorage.getItem('Token'); // Returns true if 'Token' exists, otherwise false
 };
+
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'create-account', component: CreateAccountComponent },
-  { path: 'gallery', component: ImageGalleryComponent },
-  { path: 'device', component: DeviceComponent },
-  //{
-    //path: 'components',
-    //canActivate: [checkAuth],  // Use function to protect routes
-    //children: [
-    //  { path: 'gallery', component: ImageGalleryComponent },
-    //  { path: 'device', component: DeviceComponent },
-    //],
-  //},
+  {
+    path: 'components',
+    canActivate: [checkAuth],  // Use function to protect routes
+    children: [
+      { path: 'gallery', component: ImageGalleryComponent },
+      { path: 'device', component: DeviceComponent },
+    ],
+  },
   { path: '', redirectTo: 'login', pathMatch: 'full' },  // Default to login if no route is matched
   { path: '**', redirectTo: 'login' },  // Redirect invalid routes to login
 ];
@@ -32,4 +29,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
